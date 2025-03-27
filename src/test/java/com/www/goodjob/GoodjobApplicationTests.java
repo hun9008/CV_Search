bd6b2ba8dbc68ecc1750ffb75255ffe6b60f7174
21YuJin <you5463@ajou.ac.kr>
2025-03-27 19:59:48 +0900

    First commit on dev_spring

---
+++ src/test/java/com/www/goodjob/GoodjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.goodjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
