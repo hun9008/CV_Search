2b2e41bd9f8b432ee295ab184cfebb773d55c1ea
jyHun <younghune135@gmail.com>
2025-03-31 16:20:45 +0900

    FEAT : add SwaggerUI & API docs

--- src/main/java/com/www/goodjob/config/SecurityConfig.java
+++ src/main/java/com/www/goodjob/config/SecurityConfig.java
@@ -34,7 +34,14 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/auth/**", "/oauth2/**").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/auth/**",
+                                "/oauth2/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtTokenProvider.jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
