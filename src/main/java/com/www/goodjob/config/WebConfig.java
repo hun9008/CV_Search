2c8b370715ed38b7e777653c734957f3994dbfdd
21YuJin <you5463@ajou.ac.kr>
2025-04-06 20:05:24 +0900

    FIX: http -> https

--- src/main/java/com/www/goodjob/config/WebConfig.java
+++ src/main/java/com/www/goodjob/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 프론트 도메인
+                .allowedOrigins("https://localhost:5173") // 프론트 도메인
                 // .allowedOrigins("https://goodjob.ai.kr") // 프론트 도메인
                 .allowedMethods("*")
                 .allowCredentials(true); // refreshToken 쿠키 전송 허용
