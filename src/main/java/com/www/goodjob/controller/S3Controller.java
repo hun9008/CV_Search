7c4d1cb086f598a5bf807f86c7d4e18663b05faf
jyHun <younghune135@gmail.com>
2025-04-10 15:50:03 +0900

    FEAT: s3 required Header Token

--- src/main/java/com/www/goodjob/controller/S3Controller.java
+++ src/main/java/com/www/goodjob/controller/S3Controller.java
@@ -1,5 +1,8 @@
 package com.www.goodjob.controller;
 
+import com.www.goodjob.domain.User;
+import com.www.goodjob.repository.UserRepository;
+import com.www.goodjob.service.JwtTokenProvider;
 import com.www.goodjob.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/s3")
 public class S3Controller {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
     private final S3Service s3Service;
 
     @GetMapping("/presigned-url")
@@ -20,9 +25,16 @@ public class S3Controller {
 
     @PostMapping("/confirm-upload")
     public ResponseEntity<String> confirmUpload(
-            @RequestParam Long userId,
+            @RequestHeader("Authorization") String authHeader,
             @RequestParam String fileName
     ) {
+        String token = authHeader.replace("Bearer ", "");
+        String email = jwtTokenProvider.getEmail(token);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Long userId = user.getId();
+
         boolean saved = s3Service.saveCvIfUploaded(userId, fileName);
         if (saved) {
             return ResponseEntity.ok("CV 정보가 저장되었습니다.");
