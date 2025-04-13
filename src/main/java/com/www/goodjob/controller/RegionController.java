23b683502673c3a96ce94c68f70c76eb94a13d85
21YuJin <you5463@ajou.ac.kr>
2025-04-13 16:25:08 +0900

    CHORE : comment out region-related code

--- src/main/java/com/www/goodjob/controller/RegionController.java
+++ src/main/java/com/www/goodjob/controller/RegionController.java
@@ -1,36 +1,36 @@
-package com.www.goodjob.controller;
-
-import com.www.goodjob.domain.Region;
-import com.www.goodjob.repository.RegionRepository;
-import com.www.goodjob.service.RegionService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/region")
-public class RegionController {
-
-    private final RegionService regionService;
-    private final RegionRepository regionRepository;
-
-    @PostMapping("/load")
-    public ResponseEntity<String> loadRegions(@RequestParam(required = false) String cd) {
-        regionService.fetchAndSaveRegions(cd);
-        return ResponseEntity.ok("지역 저장 완료");
-    }
-
-    @PostMapping("/load/all")
-    public ResponseEntity<String> loadAllRegions() {
-        regionService.fetchAllRegions();
-        return ResponseEntity.ok("전국 전체 지역 저장 완료");
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Region>> getAllRegions() {
-        return ResponseEntity.ok(regionRepository.findAll());
-    }
-}
+//package com.www.goodjob.controller;
+//
+//import com.www.goodjob.domain.Region;
+//import com.www.goodjob.repository.RegionRepository;
+//import com.www.goodjob.service.RegionService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/region")
+//public class RegionController {
+//
+//    private final RegionService regionService;
+//    private final RegionRepository regionRepository;
+//
+//    @PostMapping("/load")
+//    public ResponseEntity<String> loadRegions(@RequestParam(required = false) String cd) {
+//        regionService.fetchAndSaveRegions(cd);
+//        return ResponseEntity.ok("지역 저장 완료");
+//    }
+//
+//    @PostMapping("/load/all")
+//    public ResponseEntity<String> loadAllRegions() {
+//        regionService.fetchAllRegions();
+//        return ResponseEntity.ok("전국 전체 지역 저장 완료");
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<Region>> getAllRegions() {
+//        return ResponseEntity.ok(regionRepository.findAll());
+//    }
+//}
--- src/main/java/com/www/goodjob/domain/Region.java
+++ src/main/java/com/www/goodjob/domain/Region.java
@@ -1,24 +1,24 @@
-package com.www.goodjob.domain;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Table(name = "regions")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Region {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String cd;
-    private String sido;
-    private String sigungu;
-    private String xCoor;
-    private String yCoor;
-}
+//package com.www.goodjob.domain;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Entity
+//@Table(name = "regions")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class Region {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String cd;
+//    private String sido;
+//    private String sigungu;
+//    private String xCoor;
+//    private String yCoor;
+//}
