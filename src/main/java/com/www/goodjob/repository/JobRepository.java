2ce5f4f2bcbb8d48eb4edc29257a6484c19a505a
21YuJin <you5463@ajou.ac.kr>
2025-05-02 23:33:51 +0900

    FIX : resolve filtering and pagination issue

--- src/main/java/com/www/goodjob/repository/JobRepository.java
+++ src/main/java/com/www/goodjob/repository/JobRepository.java
@@ -4,18 +4,19 @@ import com.www.goodjob.domain.Job;
 import com.www.goodjob.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
     @Query("SELECT j FROM Job j WHERE j.isPublic = true " +
-            "AND (" +
-            ":keyword IS NULL OR " +
+            "AND (:keyword IS NULL OR " +
             "LOWER(j.companyName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(j.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(j.department) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
@@ -24,10 +25,8 @@ public interface JobRepository extends JpaRepository<Job, Long> {
             "LOWER(j.jobType) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(j.preferredQualifications) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(j.idealCandidate) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(j.requirements) LIKE LOWER(CONCAT('%', :keyword, '%'))" +
-            ")")
-    Page<Job> searchJobs(
-            @Param("keyword") String keyword,
-            Pageable pageable
-    );
+            "LOWER(j.requirements) LIKE LOWER(CONCAT('%', :keyword, '%')) )")
+    List<Job> searchJobs(@Param("keyword") String keyword, Sort sort);
+
 }
+
