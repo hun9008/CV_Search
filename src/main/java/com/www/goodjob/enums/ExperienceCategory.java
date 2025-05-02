d05f4ca69f0cbb6559619af027cbd4e7107c1743
21YuJin <you5463@ajou.ac.kr>
2025-05-02 17:32:24 +0900

    FEAT : simplify experience category logic

--- src/main/java/com/www/goodjob/enums/ExperienceCategory.java
+++ src/main/java/com/www/goodjob/enums/ExperienceCategory.java
@@ -4,19 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum ExperienceCategory {
-    경력무관,
-    신입,
-    _1_3년("1~3년"),
-    _4_6년("4~6년"),
-    _7_9년("7~9년"),
-    _10_15년("10~15년"),
-    _16_20년("16~20년");
+    경력무관("경력무관"),
+    신입("신입"),
+    경력("경력");
 
     private final String label;
-
-    ExperienceCategory() {
-        this.label = this.name();
-    }
 
     ExperienceCategory(String label) {
         this.label = label;
@@ -27,6 +19,8 @@ public enum ExperienceCategory {
     }
 
     public static List<String> asList() {
-        return Arrays.stream(values()).map(ExperienceCategory::getLabel).toList();
+        return Arrays.stream(values())
+                .map(ExperienceCategory::getLabel)
+                .toList();
     }
 }
