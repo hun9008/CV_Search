044d21a92eabfe407870e88e9bd67d9253592309
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-05-20 19:12:11 +0900

    FEAT: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
-import fs from 'fs';
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        https: {
-            key: fs.readFileSync('localhost-key.pem'),
-            cert: fs.readFileSync('localhost.pem'),
-        },
-        port: 5173,
-    },
     // SCSS 전역 사용
     css: {
         preprocessorOptions: {
