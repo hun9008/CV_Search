43948061fefe0bf951ea2c6932b44da5b47c0ad8
jyHun <younghune135@gmail.com>
2025-05-25 09:29:05 +0900

    Reset spring with dev_spring

--- spring/src/main/java/com/www/goodjob/enums/OAuthProvider.java
+++
@@ -1,6 +0,0 @@
-package com.www.goodjob.enums;
-
-public enum OAuthProvider {
-    GOOGLE,
-    KAKAO
-}
--- spring/src/main/java/com/www/goodjob/enums/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.www.goodjob.enums;
-
-public enum UserRole {
-    USER, ADMIN
-}
