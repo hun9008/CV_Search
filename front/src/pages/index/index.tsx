f1f1f4dd26de9c94b0d0438da6f118a5f6a304f9
jyHun <younghune135@gmail.com>
2025-05-25 09:29:09 +0900

    Reset front with dev_front

--- front/src/pages/index/index.tsx
+++
@@ -1,7 +0,0 @@
-import LandingPage from '../../pages/landing/index';
-
-function index() {
-    return <LandingPage />;
-}
-
-export default index;
--- front/src/pages/loading/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './styles/index.module.scss';
-
-function index() {
-    return <div className={styles.page}>index</div>;
-}
-
-export default index;
