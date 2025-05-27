5de5559ebc27482958a31127cbcdec2329663171
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-05-27 22:12:08 +0900

    FEAT: Fix routing method

--- src/components/common/sideBar/SideBar.tsx
+++ src/components/common/sideBar/SideBar.tsx
@@ -14,58 +14,62 @@ import usePageStore from '../../../store/pageStore';
 import style from './SideBar.module.scss';
 import useUserStore from '../../../store/userStore';
 import SideBarProfileDialog from '../dialog/SideBarProfileDialog';
+import { useEffect } from 'react';
+import useAuthStore from '../../../store/authStore';
 function SideBar() {
     const { setActiveContent } = usePageStore();
-    const { name, email } = useUserStore();
+    const { name, email, fetchUserData } = useUserStore();
+    const isAdmin = useUserStore((state) => state.isAdmin);
     const activeContent = usePageStore((state) => state.activeContent);
     const isCompactMenu = usePageStore((state) => state.isCompactMenu);
     const navigate = useNavigate();
-    type PageContent = (typeof menuItems)[number]['id'];
-    type PageSubContent = (typeof subMenuItems)[number]['id'];
+    type userPageContent = (typeof userMenuItems)[number]['id'];
+    type adminPageContent = (typeof adminMenuItems)[number]['id'];
 
-    const menuItems = [
+    const userMenuItems = [
         {
             id: '지원 관리',
-            path: '/main',
+            path: 'manage',
             icon: ClipboardList,
         },
         {
             id: '추천 공고',
-            path: '/main',
+            path: 'recommend',
             icon: Star,
         },
         {
             id: '북마크',
-            path: '/main',
+            path: 'bookmark',
             icon: Bookmark,
         },
         {
             id: '나의 CV',
-            path: '/main',
+            path: 'mycv',
             icon: User,
         },
-        {
-            id: '대시보드',
-            path: '/main',
-            icon: LayoutDashboard,
-        },
-        {
-            id: '공고 관리',
-            path: '/main',
-            icon: Briefcase,
-        },
-        { id: '피드백 관리', path: '/main', icon: Sticker },
-    ] as const; // as const를 붙이면 타입을 추론하는 것이 아니라 리터럴 자체로 고정됨
 
-    const subMenuItems = [
         {
             id: 'CV 생성',
             path: '/main',
             icon: FileUser,
         },
+    ] as const; // as const를 붙이면 타입을 추론하는 것이 아니라 리터럴 자체로 고정됨
+
+    const adminMenuItems = [
+        {
+            id: '대시보드',
+            path: 'admin/dashboard',
+            icon: LayoutDashboard,
+        },
+        {
+            id: '공고 관리',
+            path: 'admin/jobManage',
+            icon: Briefcase,
+        },
+        { id: '피드백 관리', path: 'admin/feedback', icon: Sticker },
     ] as const;
 
-    const handleMenuClick = (menuId: PageContent | PageSubContent) => {
+    const handleMenuClick = (menuId: userPageContent | adminPageContent) => {
         setActiveContent(menuId);
     };
 
@@ -73,55 +77,77 @@ function SideBar() {
         navigate('/');
     };
 
+    useEffect(() => {
+        const accessToken = useAuthStore.getState().accessToken; // 쿠키에서 가져오든, 로컬 변수든
+        if (accessToken) {
+            fetchUserData(accessToken);
+        }
+    }, []);
+
     return (
         <div className={`${style.sidebar} ${isCompactMenu ? style.sidebar__hidden : ''}`}>
             <div className={style.sidebar__container}>
-                <div className={style.sidebar__logo} onClick={handleLogoClick}>
-                    goodJob
-                </div>
+                {isAdmin ? (
+                    <div className={style.sidebar__logo}>goodJob</div>
+                ) : (
+                    <div className={style.sidebar__logo} onClick={handleLogoClick}>
+                        goodJob
+                    </div>
+                )}
+
                 <div className={style.sidebar__navigation}>
                     <ul className={style.sidebar__menu}>
-                        {menuItems.map((item) => (
-                            <li key={item.id} className={style.sidebar__menuItem}>
-                                <Link // 이후 삭제할 것
-                                    to={item.path}
-                                    className={`${style.sidebar__menuLink} ${
-                                        activeContent === item.id ? style.active : ''
-                                    }`}
-                                    onClick={() => handleMenuClick(item.id)}>
-                                    <item.icon className={style.sidebar__menuIcon} size={24} />
-                                    <span className={style.sidebar__menuText}>{item.id}</span>
-                                </Link>
-                            </li>
-                        ))}
+                        {isAdmin
+                            ? adminMenuItems.map((item) => (
+                                  <li key={item.id} className={style.sidebar__menuItem}>
+                                      <Link
+                                          to={item.path}
+                                          className={`${style.sidebar__menuLink} ${
+                                              activeContent === item.id ? style.active : ''
+                                          }`}
+                                          onClick={() => handleMenuClick(item.id)}>
+                                          <item.icon
+                                              className={style.sidebar__menuIcon}
+                                              size={24}
+                                          />
+                                          <span className={style.sidebar__menuText}>{item.id}</span>
+                                      </Link>
+                                  </li>
+                              ))
+                            : userMenuItems.map((item) => (
+                                  <li key={item.id} className={style.sidebar__menuItem}>
+                                      <Link // 이후 삭제할 것
+                                          to={item.path}
+                                          className={`${style.sidebar__menuLink} ${
+                                              activeContent === item.id ? style.active : ''
+                                          }`}
+                                          onClick={() => handleMenuClick(item.id)}>
+                                          <item.icon
+                                              className={style.sidebar__menuIcon}
+                                              size={24}
+                                          />
+                                          <span className={style.sidebar__menuText}>{item.id}</span>
+                                      </Link>
+                                  </li>
+                              ))}
                     </ul>
 
                     <div className={style.sidebar__subConatiner}>
-                        <ul className={style.sidebar__menu}>
-                            {subMenuItems.map((item) => (
-                                <li key={item.id} className={style.sidebar__menuItem}>
-                                    <Link // 이후 삭제할 것
-                                        to={item.path}
-                                        className={`${style.sidebar__menuLink} ${
-                                            activeContent === item.id ? style.active : ''
-                                        }`}
-                                        onClick={() => handleMenuClick(item.id)}>
-                                        <item.icon className={style.sidebar__menuIcon} size={24} />
-                                        <span className={style.sidebar__menuText}>{item.id}</span>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                        <div className={style.plan}>
-                            <div className={style.plan__textContainer}>
-                                <h3>
-                                    <Crown size={16} className={style.planIcon} />
-                                    베이직 플랜
-                                </h3>
-                                <p>제한적인 추천과 관리만 제공됩니다</p>
+                        {isAdmin ? (
+                            ''
+                        ) : (
+                            <div className={style.plan}>
+                                <div className={style.plan__textContainer}>
+                                    <h3>
+                                        <Crown size={16} className={style.planIcon} />
+                                        베이직 플랜
+                                    </h3>
+                                    <p>제한적인 추천과 관리만 제공됩니다</p>
+                                </div>
+                                <button className={style.plan__upgrade}>업그레이드</button>
                             </div>
-                            <button className={style.plan__upgrade}>업그레이드</button>
-                        </div>
+                        )}
+
                         <span className={style.divider}></span>
                         <div className={style.sidebar__profile}>
                             <SideBarProfileDialog />
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,47 +1,27 @@
 import styles from './styles/index.module.scss';
 import SideBar from '../../components/common/sideBar/SideBar';
 import MainHeader from '../../components/common/header/MainHeader';
-import JobList from './components/jobList/JobList';
-import MyCv from './components/myCv/MyCv';
-import Manage from './components/manage/Manage';
-import Bookmark from './components/bookmark/Bookmark';
-import JobDetail from './components/jobList/JobDetail';
 import usePageStore from '../../store/pageStore';
+import { Outlet, useLocation } from 'react-router-dom';
 
 function Index() {
-    const activeContent = usePageStore((state) => state.activeContent);
     const isCompactMenu = usePageStore((state) => state.isCompactMenu);
+    const location = useLocation();
+    const path = location.pathname;
+
+    // admin 관련 경로에서는 MainHeader를 숨김
+    const hideHeader =
+        path.includes('admin/dashboard') ||
+        path.includes('admin/jobManage') ||
+        path.includes('admin/feedback');
 
     return (
         <div className={styles.layout}>
             <SideBar />
-
             <div className={`${styles.mainContent} ${isCompactMenu ? styles.hidden : ''}`}>
-                <MainHeader />
-
+                {!hideHeader && <MainHeader />}
                 <div className={styles.mainContent__container}>
-                    {activeContent === '추천 공고' && (
-                        <div className={styles.mainContent__jobSection}>
-                            <div className={styles.mainContent__jobList}>
-                                <JobList bookmarked={false} />
-                            </div>
-
-                            <div className={styles.mainContent__jobDetail}>
-                                <JobDetail />
-                            </div>
-                        </div>
-                    )}
-                    {activeContent === '지원 관리' && <Manage />}
-                    {activeContent === '북마크' && (
-                        <div className={styles.mainContent__bookmarkList}>
-                            <Bookmark />
-
-                            {/* <div className={styles.mainContent__jobDetail}>
-                                <JobDetail />
-                            </div> */}
-                        </div>
-                    )}
-                    {activeContent === '나의 CV' && <MyCv />}
+                    <Outlet />
                 </div>
             </div>
         </div>
