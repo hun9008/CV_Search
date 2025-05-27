c784e49292251bc0811f6c20029bf3c8add8563d
jyHun <younghune135@gmail.com>
2025-05-28 00:08:53 +0900

    FEAT: add re-upload API

--- src/main/java/com/www/goodjob/controller/S3Controller.java
+++ src/main/java/com/www/goodjob/controller/S3Controller.java
@@ -1,8 +1,10 @@
 package com.www.goodjob.controller;
 
 import com.www.goodjob.security.CustomUserDetails;
+import com.www.goodjob.service.CvService;
 import com.www.goodjob.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class S3Controller {
 
     private final S3Service s3Service;
+    private final CvService cvService;
 
     @GetMapping("/presigned-url/upload")
     public ResponseEntity<String> getPresignedPutUrl(@RequestParam String fileName) {
@@ -32,6 +35,28 @@ public class S3Controller {
             @RequestParam String fileName
     ) {
         Long userId = userDetails.getId();
+
+        boolean saved = s3Service.saveCvIfUploaded(userId, fileName);
+        if (saved) {
+            return ResponseEntity.ok("CV 정보가 저장되었습니다.");
+        } else {
+            return ResponseEntity.badRequest().body("S3에 해당 파일이 존재하지 않습니다.");
+        }
+    }
+
+    @PostMapping("/confirm-re-upload")
+    public ResponseEntity<String> confirmReUpload(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam String fileName
+    ) {
+        Long userId = userDetails.getId();
+
+        try {
+            String message = cvService.deleteCv(userId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("기존 이력서 삭제 중 오류가 발생했습니다.");
+        }
 
         boolean saved = s3Service.saveCvIfUploaded(userId, fileName);
         if (saved) {
