928f2feff589d6416a266e3b7deae96c54623a8a
21YuJin <you5463@ajou.ac.kr>
2025-06-04 11:25:00 +0900

    FIX : add no-args constructor for ConfirmPaymentRequest to enable JSON deserialization

--- src/main/java/com/www/goodjob/dto/tossdto/ConfirmPaymentRequest.java
+++ src/main/java/com/www/goodjob/dto/tossdto/ConfirmPaymentRequest.java
@@ -1,8 +1,10 @@
 package com.www.goodjob.dto.tossdto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ConfirmPaymentRequest {
     private String paymentKey;
     private String orderId;
